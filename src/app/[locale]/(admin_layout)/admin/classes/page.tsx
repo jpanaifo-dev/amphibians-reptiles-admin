e94a711be6515286adba2f376f4bab204a07d36b
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-12-11 23:20:01 -0500

    feat: agregar propiedad 'type' a HeatmapQuestionType y mejorar la gestión de tipos en HeatmapQuestionTypeChart

--- src/app/[locale]/(admin_layout)/admin/classes/page.tsx
+++ src/app/[locale]/(admin_layout)/admin/classes/page.tsx
@@ -1,42 +1,34 @@
-import { PaginationCustom } from '@/components/app/pagination-custom'
-import { ClassList } from '@/modules/taxonomies/classes/components/class-list'
-import { classService } from '@/modules/taxonomies/classes/services/class.service'
-import { SearchParams } from '@/types/core';
+import { PaginationCustom } from "@/components/app/pagination-custom";
+import { ClassList } from "@/modules/taxonomies/classes/components/class-list";
+import { classService } from "@/modules/taxonomies/classes/services/class.service";
+import { SearchParams } from "@/types/core";
 
 interface PageProps {
-    searchParams: SearchParams
+  searchParams: SearchParams;
 }
 
 export default async function Page(props: PageProps) {
-    const searchParams = await props.searchParams;
-    const params = await searchParams;
-    const page = params?.page ? parseInt(params.page as string, 10) : 1
-    const limit = params?.pageSize ? parseInt(params.pageSize as string, 10) : 10
-    const query = params?.name as string || params?.query as string || ''
+  const searchParams = await props.searchParams;
+  const params = await searchParams;
+  const page = params?.page ? parseInt(params.page as string, 10) : 1;
+  const limit = params?.pageSize ? parseInt(params.pageSize as string, 10) : 10;
+  const query = (params?.name as string) || (params?.query as string) || "";
 
-    try {
-        const response = await classService.getAll({
-            name: query,
-            page,
-            pageSize: limit,
-        });
+  const response = await classService.getAll({
+    page,
+    pageSize: limit,
+    query,
+  });
 
-        return (
-            <>
-                <ClassList items={response.data || []} />
-                <PaginationCustom
-                    totalPages={response.totalPages || 1}
-                    page={response.page || 1}
-                    limit={limit}
-                    total={response.total || 0}
-                />
-            </>
-        )
-    } catch (error) {
-        return (
-            <div className="text-center py-10 text-red-500">
-                Error al cargar los datos. Por favor intente nuevamente.
-            </div>
-        )
-    }
+  return (
+    <>
+      <ClassList items={response.data || []} />
+      <PaginationCustom
+        totalPages={response.totalPages || 1}
+        page={response.page || 1}
+        limit={limit}
+        total={response.total || 0}
+      />
+    </>
+  );
 }
