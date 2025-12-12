dcfc4d00feb056fb611c0697f8fc60c89f7c440e
DESKTOP-DCPA18G\Dayler <daylersan@gmail.com>
2025-12-11 21:59:47 -0500

    feat: actualizar el componente SimulacroBuilder para soportar múltiples especialidades y mejorar la gestión de filtros

--- src/app/[locale]/(admin_layout)/admin/classes/page.tsx
+++ src/app/[locale]/(admin_layout)/admin/classes/page.tsx
@@ -1,13 +1,14 @@
 import { PaginationCustom } from '@/components/app/pagination-custom'
 import { ClassList } from '@/modules/taxonomies/classes/components/class-list'
 import { classService } from '@/modules/taxonomies/classes/services/class.service'
-import { SearchParams } from '@/lib/definitions' // Make sure SearchParams is compatible or redefine locally
+import { SearchParams } from '@/types/core';
 
 interface PageProps {
-    searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+    searchParams: SearchParams
 }
 
-export default async function Page({ searchParams }: PageProps) {
+export default async function Page(props: PageProps) {
+    const searchParams = await props.searchParams;
     const params = await searchParams;
     const page = params?.page ? parseInt(params.page as string, 10) : 1
     const limit = params?.pageSize ? parseInt(params.pageSize as string, 10) : 10
--- src/types/core/pages.ts
+++ src/types/core/pages.ts
@@ -1,4 +1,5 @@
 export type Params = Promise<{ [key: string]: string | string[] | undefined }>
+
 export type SearchParams = Promise<{
   [key: string]: string | string[] | undefined
 }>
